90053cd3a6358edb74544dbdd6456fd295c49ae9
Mariam <mariamnaz212@gmail.com>
2020-12-09 01:58:09 +0500

    done templating

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\AdminController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::view('/' , 'welcome');
+
+Route::match(['get','post'],'/',[IndexController::class, 'index']);
+Route::match(['get', 'post'], '/login', [AdminController::class, 'login']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
