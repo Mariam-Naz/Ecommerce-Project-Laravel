747856426334aa740a7a646f005e8fec088e32a0
MaazShah2060 <MaazShah2060>
2020-12-30 00:19:59 +0500

    Dynamic categories on index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Banner;
+use App\Models\Category;
 
 class IndexController extends Controller
 {
     public function index(){
         $banners = Banner::where('status' , 1)->orderby('sortOrder' , 'asc')->get();
-        return view('maryaaz.index')->with(compact('banners'));
+        $categories = Category::with('categories')->where(['parentId'=>0])->get();
+        return view('maryaaz.index')->with(compact('banners','categories'));
     }
 }
