8bab3f129b9b664d102510b70579086bb020c7f8
MaazShah2060 <MaazShah2060>
2020-12-31 01:16:34 +0500

    Dynamic Products

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Banner;
 use App\Models\Category;
+use App\Models\Products;
 
 class IndexController extends Controller
 {
     public function index(){
         $banners = Banner::where('status' , 1)->orderby('sortOrder' , 'asc')->get();
         $categories = Category::with('categories')->where(['parentId'=>0])->get();
-        return view('maryaaz.index')->with(compact('banners','categories'));
+        $products = Products::get();
+        return view('maryaaz.index')->with(compact('banners','categories','products'));
     }
 }
